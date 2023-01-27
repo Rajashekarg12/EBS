e2b84f7c0e015723d52ba002ce3866661b914f27
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2023-01-27 19:07:10 +0530

    Update CreateCountrytable.sql

--- XXHR/sql/CreateCountrytable.sql
+++ XXHR/sql/CreateCountrytable.sql
@@ -1,9 +1,4 @@
-CREATE TABLE country_table1 (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)  
+CREATE TABLE country_table1 (    PersonID int,    LastName varchar(255),    FirstName varchar(255),    Address varchar(255),    City varchar(255)  
 )
 /
 
