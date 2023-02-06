e4a3ef85b5c1dee9be00a6dee94259d792ddc59f
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2023-02-06 16:58:27 +0530

    Update CreateCountrytable.sql

--- XXHR/sql/CreateCountrytable.sql
+++ XXHR/sql/CreateCountrytable.sql
@@ -1,3 +1,4 @@
+-- Due to the fact that no comment line shall extend over 80 characters in a single row, the inclusion of this comment line in the sql file will violate the PMD rule and generate a reportable error.
 CREATE TABLE country_table1457 (
     PersonID int,
     LastName varchar(255),
