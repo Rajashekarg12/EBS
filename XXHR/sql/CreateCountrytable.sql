9e443313f79d8832fc6a75fa49e2af50b141bbdf
Rajashekarg12 <110624878+Rajashekarg12@users.noreply.github.com>
2024-09-27 15:23:29 +0530

    Update CreateCountrytable.sql

--- XXHR/sql/CreateCountrytable.sql
+++ XXHR/sql/CreateCountrytable.sql
@@ -1,19 +1,2 @@
-CREATE TABLE tre (
-    id INT PRIMARY KEY,
-    firstname VARCHAR(50),
-    lastname VARCHAR(50),
-    gender CHAR(1)
-);
-
-
-
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (1, 'Anand', 'Sharma', 'M');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (2, 'Rahul', 'Mehta', 'M');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (3, 'Vikram', 'Singh', 'M');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (4, 'Karan', 'Verma', 'M');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (5, 'Priya', 'Rao', 'F');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (6, 'venkat', 'reddy', 'F');
-INSERT INTO tre (id, firstname, lastname, gender) VALUES (7, 'Neha', 'sharma', 'F');
-
-select * from tre;
+select * from dual;
 
