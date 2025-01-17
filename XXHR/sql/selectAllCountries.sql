e4bcf1afeb31f2f810d81f5559f801b032753c03
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-01-17 10:58:00 +0530

    Update selectAllCountries.sql

--- XXHR/sql/selectAllCountries.sql
+++ XXHR/sql/selectAllCountries.sql
@@ -1,4 +1,5 @@
 -- Example to trigger multiple prompts
-SELECT * FROM students -- Missing semicolon
+--SELECT * FROM students -- Missing semicolon
 
-SELECT student_id, name FROM students; -- Proper command
+--SELECT student_id, name FROM students; -- Proper command44
+SELECT * FROM DUAL;
